e1b7c04744fa834e274b5d9f549153e1978259b3
Zach Bernstein <zebernst@gmail.com>
2019-12-16 12:26:17 -0600

    Solved 1.2

--- solutions/Day01/solution.py
+++ solutions/Day01/solution.py
@@ -1,19 +1,31 @@
 from aoc import AOCProblem
 
 
+def calculate_fuel(module):
+    return module // 3 - 2
+
+
 class Day01(AOCProblem):
     day = 1
 
     def part1(self):
         modules = [int(m) for m in self.data.splitlines()]
 
-        def calculate_fuel(module):
-            return module // 3 - 2
-
         return sum(calculate_fuel(m) for m in modules)
 
     def part2(self):
-        pass
+        modules = [int(m) for m in self.data.splitlines()]
+
+        total = 0
+        for mod in modules:
+            subtotal = 0
+            result = calculate_fuel(mod)
+            while result > 0:
+                subtotal += result
+                result = calculate_fuel(result)
+            total += subtotal
+
+        return total
 
 
 if __name__ == '__main__':
