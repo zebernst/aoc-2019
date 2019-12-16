fa88d1133a300a8dad91a0bf8bb5ea0741c8e10c
Zach Bernstein <zebernst@gmail.com>
2019-12-16 13:59:11 -0600

    Solved 2.2

--- solutions/Day02/solution.py
+++ solutions/Day02/solution.py
@@ -14,7 +14,7 @@ class Day02(AOCProblem):
         return [int(i) for i in data.split(',')]
 
     def part1(self):
-        intcode = self.data
+        intcode = self.data[:]
         intcode[1] = 12
         intcode[2] = 2
         idx = 0
@@ -34,7 +34,27 @@ class Day02(AOCProblem):
         return intcode[0]
 
     def part2(self):
-        pass
+        for noun in range(100):
+            for verb in range(100):
+                intcode = self.data[:]
+                intcode[1] = noun
+                intcode[2] = verb
+                idx = 0
+                while intcode[idx] != 99:
+                    opcode = intcode[idx]
+                    pos1 = intcode[idx + 1]
+                    pos2 = intcode[idx + 2]
+                    outpos = intcode[idx + 3]
+
+                    val1 = intcode[pos1]
+                    val2 = intcode[pos2]
+
+                    result = opcodes.get(opcode)(val1, val2)
+                    intcode[outpos] = result
+                    idx += 4
+
+                if intcode[0] == 19690720:
+                    return 100 * noun + verb
 
 
 if __name__ == '__main__':
